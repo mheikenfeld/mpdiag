56622f6816fcb995ffd5aade5fbf2b789d7d6fa1
Max Heikenfeld <maxheikenfeld@web.de>
2020-03-31 18:49:00 +0200

    Update setup.py: License, version based on github tag, author_email

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
 setup(name='mpdiag',
-      version='0.8',
+      use_scm_version=True,
+      setup_requires=['setuptools_scm'],
       description='Load and process WRF and RAMS microphysics diagnostics output',
       url='http://github.com/mheikenfeld/mpdiag',
       author='Max Heikenfeld',
-      author_email='max.heikenfeld@physics.ox.ac.uk',
-      license='GNU',
+      author_email='max.heikenfeld@web.de',
+      license='BSD-3-Clause',
       packages=['mpdiag'],
-      install_requires=['wrfcube','ramscubE'],
+      install_requires=['wrfcube','ramscube'],
       zip_safe=False)
